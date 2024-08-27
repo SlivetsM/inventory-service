b5f688b47829d957b2d2910cfdd852ca74ada56a
“Marat_Slivets <slivetsm@gmail.com>
2024-08-27 12:49:33 +0300

    initial commit

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE t_inventory (
+    id BIGINT(20) AUTO_INCREMENT NOT NULL,
+    sku_code VARCHAR(255) DEFAULT NULL,
+    quantity INT(11) DEFAULT NULL,
+    PRIMARY KEY (id)
+);
---
+++ src/main/resources/db/migration/V2__add_inventory.sql
@@ -0,0 +1,4 @@
+insert into t_inventory (quantity, sku_code) values (100, 'iphone-13');
+insert into t_inventory (quantity, sku_code) values (100, 'iphone-13-pro');
+insert into t_inventory (quantity, sku_code) values (100, 'iphone-12-pro');
+insert into t_inventory (quantity, sku_code) values (100, 'iphone-10-pro');
